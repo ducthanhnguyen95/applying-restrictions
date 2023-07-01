d712db54e0fe76ea731d14feddd8e098d2990672
thanhnd <ducthanhnguyen95@gmail.com>
2023-07-01 22:37:00 +0700

    Applying restrictions : using matcher methods to select endpoints

---
+++ src/test/java/com/example/applying/restrictions/ApplyingRestrictionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applying.restrictions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyingRestrictionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
