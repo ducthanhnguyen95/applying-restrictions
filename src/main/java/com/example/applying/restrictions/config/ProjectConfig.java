0287d2f9c226e4b6e58c2637981dcfbcf9e8c975
thanhnd <ducthanhnguyen95@gmail.com>
2023-07-04 05:27:10 +0700

    Applying restrictions: selecting requests to apply authorization restrictions

--- src/main/java/com/example/applying/restrictions/config/ProjectConfig.java
+++ src/main/java/com/example/applying/restrictions/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.example.applying.restrictions.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -36,14 +37,24 @@ public class ProjectConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.httpBasic(Customizer.withDefaults());
-//        http.authorizeHttpRequests(c -> c
-//                .requestMatchers("/hello").hasRole("ADMIN")
-//                .requestMatchers("/ciao").hasRole("MANAGER").anyRequest().permitAll());
 
-        http.authorizeHttpRequests(c -> c
-                .requestMatchers("/hello").hasRole("ADMIN")
-                .requestMatchers("/ciao").hasRole("MANAGER").anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(
+                c -> c.requestMatchers(HttpMethod.GET, "/a")
+                        .authenticated()
+                        .requestMatchers(HttpMethod.POST, "/a")
+                        .permitAll()
+                        .requestMatchers("/a/b/**")
+                        .authenticated()
+                        .requestMatchers("/product/{code:^[0-9]*$}")
+                        .permitAll()
+                        .anyRequest()
+                        .denyAll()
+
+        );
+        http.csrf(c -> c.disable());
         return http.build();
+
     }
 }
